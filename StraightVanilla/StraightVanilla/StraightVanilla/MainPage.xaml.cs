0294ff7afbefb175cc22b37e240649fd8ece14f6
Giannis Papadopoulos <papadopoulos.ioannis@nbg.gr>
2021-10-26 19:07:01 +0300

    Add sample code.

--- StraightVanilla/StraightVanilla/StraightVanilla/MainPage.xaml.cs
+++ StraightVanilla/StraightVanilla/StraightVanilla/MainPage.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace StraightVanilla
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Shell
     {
         public MainPage()
         {
